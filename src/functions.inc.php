ef8f9d34fa47f7a1c43a0c64dfc250388c767271
jgauthi <jgauthi@users.noreply.github.com>
2020-10-10 12:29:15 +0200

    [archive] 2010-2019: PHP Functions

---
+++ src/functions.inc.php
@@ -0,0 +1,362 @@
+<?php
+/**
+ * @param mixed $var
+ * @return bool
+ */
+function is_vide(&$var)
+{
+    return (null === $var || '' === trim($var));
+}
+
+/**
+ * @param mixed ...$args
+ * @return bool
+ */
+function is_vides()
+{
+    $args = func_get_args();
+
+    foreach ($args as &$var) {
+        if (is_vide($var)) {
+            continue;
+        }
+
+        return	false;
+    }
+
+    return true;
+}
+
+/**
+ * @param mixed ...$args
+ * @return bool
+ */
+function is_empties()
+{
+    $args = func_get_args();
+
+    foreach ($args as &$var) {
+        if (empty($var)) {
+            return true;
+        }
+
+        continue;
+    }
+
+    return false;
+}
+
+/**
+ * @param string $mail
+ * @return bool
+ */
+if (function_exists('valid_email_address')) { // Fonction de base de drupal 7
+    function is_email($mail)
+    {
+        // https://api.drupal.org/api/drupal/includes!common.inc/function/valid_email_address/7
+        return valid_email_address($mail);
+    }
+} elseif (function_exists('filter_var')) { // php5.2+
+    function is_email($mail)
+    {
+        return (bool) (filter_var($mail, FILTER_VALIDATE_EMAIL));
+    }
+} else {
+    function is_email($mail)
+    {
+        $qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
+        $dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
+        $atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c'.
+            '\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
+        $quoted_pair = '\\x5c[\\x00-\\x7f]';
+        $domain_literal = "\\x5b($dtext|$quoted_pair)*\\x5d";
+        $quoted_string = "\\x22($qtext|$quoted_pair)*\\x22";
+        $domain_ref = $atom;
+        $sub_domain = "($domain_ref|$domain_literal)";
+        $word = "($atom|$quoted_string)";
+        $domain = "$sub_domain(\\x2e$sub_domain)*";
+        $local_part = "$word(\\x2e$word)*";
+        $addr_spec = "$local_part\\x40$domain";
+
+        return preg_match("!^$addr_spec$!", $mail);
+    }
+}
+
+/**
+ * Preg match with a array pattern.
+ *
+ * @param string|array $pattern
+ * @return bool
+ */
+function preg_match_pattern($pattern, $subject, &$matches = [])
+{
+    if (!is_array($pattern)) {
+        return preg_match($pattern, $subject, $matches);
+    }
+
+    foreach ($pattern as $regexp) {
+        if (!preg_match($regexp, $subject, $matches)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+/**
+ * Function to implode With key + value.
+ *
+ * @param string $line separator between data
+ * @param array $data
+ * @param string $separator (optional, default =) separator between key and value
+ * @param string|null $firstline (optional, default null) string in first line
+ *
+ * @return string|null
+ */
+function implode_with_index($line, $data, $separator = '=', $firstline = null)
+{
+    if (!is_array($data) || 0 === count($data)) {
+        return null;
+    }
+
+    foreach ($data as $id => $value) {
+        if (!isset($chaine)) {
+            $chaine = $firstline;
+        } else {
+            $chaine .= $line;
+        }
+
+        $chaine .= $id.$separator.$value;
+    }
+
+    return $chaine;
+}
+
+/**
+ * Fix unserialize (Result conflict with ISO and Accents)
+ * From: https://gist.github.com/jgauthi/79f7c3a2a39f4614681e70e6f483fb5e
+ * @param string $data
+ * @return mixed
+ */
+function mb_unserialize($data)
+{
+    // Fix nb_char on php variable
+    $out = preg_replace_callback(
+        '#s:(\d+):"(.*?)";#s',
+        function ($row) { return sprintf('s:%s:"%s";', mb_strlen($row[2]), $row[2]); },
+        $data
+    );
+    $out = @unserialize($out);
+
+    // fix ISO accent
+    if (empty($out)) {
+        $out = preg_replace_callback(
+            '!s:(\d+):"(.*?)";!s',
+            function($m){
+                $len = strlen($m[2]);
+                return "s:$len:\"{$m[2]}\";";
+            },
+            $data);
+
+        $out = @unserialize($out);
+    }
+
+    if (empty($out)) {
+        $out = unserialize($data);
+    }
+
+    return $out;
+}
+
+/**
+ * @param string|null $txt
+ * @param string $charset
+ * @return string
+ */
+function htmltxt($txt, $charset = 'UTF-8')
+{
+    return htmlentities($txt, ENT_QUOTES, $charset);
+}
+
+/**
+ * Encode specials chars only (useful for mailto:mailto:?subject={$title}&body={$content}).
+ * @param string $text
+ * @param string $charset
+ * @return string
+ */
+function urlencode_entities($text, $charset = 'UTF-8')
+{
+    static $chars = null, $replace = null;
+
+    if (null === $chars) {
+        $chars = get_html_translation_table(HTML_ENTITIES, ENT_NOQUOTES, $charset);
+        $chars = array_keys($chars);
+        array_unshift($chars, '%', "\n");
+
+        $replace = array_map(function ($char) { return urlencode($char); }, $chars);
+    }
+
+    $text = str_replace($chars, $replace, $text);
+
+    return $text;
+}
+
+/**
+ * Unaccent the input string string. An example string like `ÀØėÿᾜὨζὅБю`
+ * will be translated to `AOeyIOzoBY`. More complete than :
+ *   strtr( (string)$str,
+ *          "ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ",
+ *          "aaaaaaaaaaaaooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn" );
+ *
+ * @param $str string
+ * @param $utf8 bool, function will detect input string encoding
+ * @return string input string without accent
+ */
+if (!defined('WORDPRESS_SCRIPT')) {
+    function remove_accents($str, $utf8 = true)
+    {
+        $str = (string) $str;
+        if (null === $utf8) {
+            if (!function_exists('mb_detect_encoding')) {
+                $length = mb_strlen($str);
+                $utf8 = true;
+                for ($i = 0; $i < $length; ++$i) {
+                    $c = ord($str[$i]);
+                    if ($c < 0x80) {
+                        $n = 0; // 0bbbbbbb
+                    } elseif (0xC0 === ($c & 0xE0)) {
+                        $n = 1; // 110bbbbb
+                    } elseif (0xE0 === ($c & 0xF0)) {
+                        $n = 2; // 1110bbbb
+                    } elseif (0xF0 === ($c & 0xF8)) {
+                        $n = 3; // 11110bbb
+                    } elseif (0xF8 === ($c & 0xFC)) {
+                        $n = 4; // 111110bb
+                    } elseif (0xFC === ($c & 0xFE)) {
+                        $n = 5; // 1111110b
+                    } else {
+                        return false; // Does not match any model
+                    }
+                    for ($j = 0; $j < $n; ++$j) {
+                        // n bytes matching 10bbbbbb follow ?
+                        if ((++$i === $length) || (0x80 !== (ord($str[$i]) & 0xC0))) {
+                            $utf8 = false;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                $utf8 = ('utf-8' === mb_strtolower(mb_detect_encoding($str, mb_detect_order(), true)));
+            }
+        }
+
+        if (!$utf8) {
+            $str = utf8_encode($str);
+        }
+
+        $transliteration = [
+            'Ĳ' => 'I', 'Ö' => 'O', 'Œ' => 'O', 'Ü' => 'U', 'ä' => 'a', 'æ' => 'a',
+            'ĳ' => 'i', 'ö' => 'o', 'œ' => 'o', 'ü' => 'u', 'ß' => 's', 'ſ' => 's',
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A',
+            'Æ' => 'A', 'Ā' => 'A', 'Ą' => 'A', 'Ă' => 'A', 'Ç' => 'C', 'Ć' => 'C',
+            'Č' => 'C', 'Ĉ' => 'C', 'Ċ' => 'C', 'Ď' => 'D', 'Đ' => 'D', 'È' => 'E',
+            'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ē' => 'E', 'Ę' => 'E', 'Ě' => 'E',
+            'Ĕ' => 'E', 'Ė' => 'E', 'Ĝ' => 'G', 'Ğ' => 'G', 'Ġ' => 'G', 'Ģ' => 'G',
+            'Ĥ' => 'H', 'Ħ' => 'H', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'Ī' => 'I', 'Ĩ' => 'I', 'Ĭ' => 'I', 'Į' => 'I', 'İ' => 'I', 'Ĵ' => 'J',
+            'Ķ' => 'K', 'Ľ' => 'K', 'Ĺ' => 'K', 'Ļ' => 'K', 'Ŀ' => 'K', 'Ł' => 'L',
+            'Ñ' => 'N', 'Ń' => 'N', 'Ň' => 'N', 'Ņ' => 'N', 'Ŋ' => 'N', 'Ò' => 'O',
+            'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ø' => 'O', 'Ō' => 'O', 'Ő' => 'O',
+            'Ŏ' => 'O', 'Ŕ' => 'R', 'Ř' => 'R', 'Ŗ' => 'R', 'Ś' => 'S', 'Ş' => 'S',
+            'Ŝ' => 'S', 'Ș' => 'S', 'Š' => 'S', 'Ť' => 'T', 'Ţ' => 'T', 'Ŧ' => 'T',
+            'Ț' => 'T', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ū' => 'U', 'Ů' => 'U',
+            'Ű' => 'U', 'Ŭ' => 'U', 'Ũ' => 'U', 'Ų' => 'U', 'Ŵ' => 'W', 'Ŷ' => 'Y',
+            'Ÿ' => 'Y', 'Ý' => 'Y', 'Ź' => 'Z', 'Ż' => 'Z', 'Ž' => 'Z', 'à' => 'a',
+            'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ā' => 'a', 'ą' => 'a', 'ă' => 'a',
+            'å' => 'a', 'ç' => 'c', 'ć' => 'c', 'č' => 'c', 'ĉ' => 'c', 'ċ' => 'c',
+            'ď' => 'd', 'đ' => 'd', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e',
+            'ē' => 'e', 'ę' => 'e', 'ě' => 'e', 'ĕ' => 'e', 'ė' => 'e', 'ƒ' => 'f',
+            'ĝ' => 'g', 'ğ' => 'g', 'ġ' => 'g', 'ģ' => 'g', 'ĥ' => 'h', 'ħ' => 'h',
+            'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ī' => 'i', 'ĩ' => 'i',
+            'ĭ' => 'i', 'į' => 'i', 'ı' => 'i', 'ĵ' => 'j', 'ķ' => 'k', 'ĸ' => 'k',
+            'ł' => 'l', 'ľ' => 'l', 'ĺ' => 'l', 'ļ' => 'l', 'ŀ' => 'l', 'ñ' => 'n',
+            'ń' => 'n', 'ň' => 'n', 'ņ' => 'n', 'ŉ' => 'n', 'ŋ' => 'n', 'ò' => 'o',
+            'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ø' => 'o', 'ō' => 'o', 'ő' => 'o',
+            'ŏ' => 'o', 'ŕ' => 'r', 'ř' => 'r', 'ŗ' => 'r', 'ś' => 's', 'š' => 's',
+            'ť' => 't', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ū' => 'u', 'ů' => 'u',
+            'ű' => 'u', 'ŭ' => 'u', 'ũ' => 'u', 'ų' => 'u', 'ŵ' => 'w', 'ÿ' => 'y',
+            'ý' => 'y', 'ŷ' => 'y', 'ż' => 'z', 'ź' => 'z', 'ž' => 'z', 'Α' => 'A',
+            'Ά' => 'A', 'Ἀ' => 'A', 'Ἁ' => 'A', 'Ἂ' => 'A', 'Ἃ' => 'A', 'Ἄ' => 'A',
+            'Ἅ' => 'A', 'Ἆ' => 'A', 'Ἇ' => 'A', 'ᾈ' => 'A', 'ᾉ' => 'A', 'ᾊ' => 'A',
+            'ᾋ' => 'A', 'ᾌ' => 'A', 'ᾍ' => 'A', 'ᾎ' => 'A', 'ᾏ' => 'A', 'Ᾰ' => 'A',
+            'Ᾱ' => 'A', 'Ὰ' => 'A', 'ᾼ' => 'A', 'Β' => 'B', 'Γ' => 'G', 'Δ' => 'D',
+            'Ε' => 'E', 'Έ' => 'E', 'Ἐ' => 'E', 'Ἑ' => 'E', 'Ἒ' => 'E', 'Ἓ' => 'E',
+            'Ἔ' => 'E', 'Ἕ' => 'E', 'Ὲ' => 'E', 'Ζ' => 'Z', 'Η' => 'I', 'Ή' => 'I',
+            'Ἠ' => 'I', 'Ἡ' => 'I', 'Ἢ' => 'I', 'Ἣ' => 'I', 'Ἤ' => 'I', 'Ἥ' => 'I',
+            'Ἦ' => 'I', 'Ἧ' => 'I', 'ᾘ' => 'I', 'ᾙ' => 'I', 'ᾚ' => 'I', 'ᾛ' => 'I',
+            'ᾜ' => 'I', 'ᾝ' => 'I', 'ᾞ' => 'I', 'ᾟ' => 'I', 'Ὴ' => 'I', 'ῌ' => 'I',
+            'Θ' => 'T', 'Ι' => 'I', 'Ί' => 'I', 'Ϊ' => 'I', 'Ἰ' => 'I', 'Ἱ' => 'I',
+            'Ἲ' => 'I', 'Ἳ' => 'I', 'Ἴ' => 'I', 'Ἵ' => 'I', 'Ἶ' => 'I', 'Ἷ' => 'I',
+            'Ῐ' => 'I', 'Ῑ' => 'I', 'Ὶ' => 'I', 'Κ' => 'K', 'Λ' => 'L', 'Μ' => 'M',
+            'Ν' => 'N', 'Ξ' => 'K', 'Ο' => 'O', 'Ό' => 'O', 'Ὀ' => 'O', 'Ὁ' => 'O',
+            'Ὂ' => 'O', 'Ὃ' => 'O', 'Ὄ' => 'O', 'Ὅ' => 'O', 'Ὸ' => 'O', 'Π' => 'P',
+            'Ρ' => 'R', 'Ῥ' => 'R', 'Σ' => 'S', 'Τ' => 'T', 'Υ' => 'Y', 'Ύ' => 'Y',
+            'Ϋ' => 'Y', 'Ὑ' => 'Y', 'Ὓ' => 'Y', 'Ὕ' => 'Y', 'Ὗ' => 'Y', 'Ῠ' => 'Y',
+            'Ῡ' => 'Y', 'Ὺ' => 'Y', 'Φ' => 'F', 'Χ' => 'X', 'Ψ' => 'P', 'Ω' => 'O',
+            'Ώ' => 'O', 'Ὠ' => 'O', 'Ὡ' => 'O', 'Ὢ' => 'O', 'Ὣ' => 'O', 'Ὤ' => 'O',
+            'Ὥ' => 'O', 'Ὦ' => 'O', 'Ὧ' => 'O', 'ᾨ' => 'O', 'ᾩ' => 'O', 'ᾪ' => 'O',
+            'ᾫ' => 'O', 'ᾬ' => 'O', 'ᾭ' => 'O', 'ᾮ' => 'O', 'ᾯ' => 'O', 'Ὼ' => 'O',
+            'ῼ' => 'O', 'α' => 'a', 'ά' => 'a', 'ἀ' => 'a', 'ἁ' => 'a', 'ἂ' => 'a',
+            'ἃ' => 'a', 'ἄ' => 'a', 'ἅ' => 'a', 'ἆ' => 'a', 'ἇ' => 'a', 'ᾀ' => 'a',
+            'ᾁ' => 'a', 'ᾂ' => 'a', 'ᾃ' => 'a', 'ᾄ' => 'a', 'ᾅ' => 'a', 'ᾆ' => 'a',
+            'ᾇ' => 'a', 'ὰ' => 'a', 'ᾰ' => 'a', 'ᾱ' => 'a', 'ᾲ' => 'a', 'ᾳ' => 'a',
+            'ᾴ' => 'a', 'ᾶ' => 'a', 'ᾷ' => 'a', 'β' => 'b', 'γ' => 'g', 'δ' => 'd',
+            'ε' => 'e', 'έ' => 'e', 'ἐ' => 'e', 'ἑ' => 'e', 'ἒ' => 'e', 'ἓ' => 'e',
+            'ἔ' => 'e', 'ἕ' => 'e', 'ὲ' => 'e', 'ζ' => 'z', 'η' => 'i', 'ή' => 'i',
+            'ἠ' => 'i', 'ἡ' => 'i', 'ἢ' => 'i', 'ἣ' => 'i', 'ἤ' => 'i', 'ἥ' => 'i',
+            'ἦ' => 'i', 'ἧ' => 'i', 'ᾐ' => 'i', 'ᾑ' => 'i', 'ᾒ' => 'i', 'ᾓ' => 'i',
+            'ᾔ' => 'i', 'ᾕ' => 'i', 'ᾖ' => 'i', 'ᾗ' => 'i', 'ὴ' => 'i', 'ῂ' => 'i',
+            'ῃ' => 'i', 'ῄ' => 'i', 'ῆ' => 'i', 'ῇ' => 'i', 'θ' => 't', 'ι' => 'i',
+            'ί' => 'i', 'ϊ' => 'i', 'ΐ' => 'i', 'ἰ' => 'i', 'ἱ' => 'i', 'ἲ' => 'i',
+            'ἳ' => 'i', 'ἴ' => 'i', 'ἵ' => 'i', 'ἶ' => 'i', 'ἷ' => 'i', 'ὶ' => 'i',
+            'ῐ' => 'i', 'ῑ' => 'i', 'ῒ' => 'i', 'ῖ' => 'i', 'ῗ' => 'i', 'κ' => 'k',
+            'λ' => 'l', 'μ' => 'm', 'ν' => 'n', 'ξ' => 'k', 'ο' => 'o', 'ό' => 'o',
+            'ὀ' => 'o', 'ὁ' => 'o', 'ὂ' => 'o', 'ὃ' => 'o', 'ὄ' => 'o', 'ὅ' => 'o',
+            'ὸ' => 'o', 'π' => 'p', 'ρ' => 'r', 'ῤ' => 'r', 'ῥ' => 'r', 'σ' => 's',
+            'ς' => 's', 'τ' => 't', 'υ' => 'y', 'ύ' => 'y', 'ϋ' => 'y', 'ΰ' => 'y',
+            'ὐ' => 'y', 'ὑ' => 'y', 'ὒ' => 'y', 'ὓ' => 'y', 'ὔ' => 'y', 'ὕ' => 'y',
+            'ὖ' => 'y', 'ὗ' => 'y', 'ὺ' => 'y', 'ῠ' => 'y', 'ῡ' => 'y', 'ῢ' => 'y',
+            'ῦ' => 'y', 'ῧ' => 'y', 'φ' => 'f', 'χ' => 'x', 'ψ' => 'p', 'ω' => 'o',
+            'ώ' => 'o', 'ὠ' => 'o', 'ὡ' => 'o', 'ὢ' => 'o', 'ὣ' => 'o', 'ὤ' => 'o',
+            'ὥ' => 'o', 'ὦ' => 'o', 'ὧ' => 'o', 'ᾠ' => 'o', 'ᾡ' => 'o', 'ᾢ' => 'o',
+            'ᾣ' => 'o', 'ᾤ' => 'o', 'ᾥ' => 'o', 'ᾦ' => 'o', 'ᾧ' => 'o', 'ὼ' => 'o',
+            'ῲ' => 'o', 'ῳ' => 'o', 'ῴ' => 'o', 'ῶ' => 'o', 'ῷ' => 'o', 'А' => 'A',
+            'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'E',
+            'Ж' => 'Z', 'З' => 'Z', 'И' => 'I', 'Й' => 'I', 'К' => 'K', 'Л' => 'L',
+            'М' => 'M', 'Н' => 'N', 'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S',
+            'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'K', 'Ц' => 'T', 'Ч' => 'C',
+            'Ш' => 'S', 'Щ' => 'S', 'Ы' => 'Y', 'Э' => 'E', 'Ю' => 'Y', 'Я' => 'Y',
+            'а' => 'A', 'б' => 'B', 'в' => 'V', 'г' => 'G', 'д' => 'D', 'е' => 'E',
+            'ё' => 'E', 'ж' => 'Z', 'з' => 'Z', 'и' => 'I', 'й' => 'I', 'к' => 'K',
+            'л' => 'L', 'м' => 'M', 'н' => 'N', 'о' => 'O', 'п' => 'P', 'р' => 'R',
+            'с' => 'S', 'т' => 'T', 'у' => 'U', 'ф' => 'F', 'х' => 'K', 'ц' => 'T',
+            'ч' => 'C', 'ш' => 'S', 'щ' => 'S', 'ы' => 'Y', 'э' => 'E', 'ю' => 'Y',
+            'я' => 'Y', 'ð' => 'd', 'Ð' => 'D', 'þ' => 't', 'Þ' => 'T', 'ა' => 'a',
+            'ბ' => 'b', 'გ' => 'g', 'დ' => 'd', 'ე' => 'e', 'ვ' => 'v', 'ზ' => 'z',
+            'თ' => 't', 'ი' => 'i', 'კ' => 'k', 'ლ' => 'l', 'მ' => 'm', 'ნ' => 'n',
+            'ო' => 'o', 'პ' => 'p', 'ჟ' => 'z', 'რ' => 'r', 'ს' => 's', 'ტ' => 't',
+            'უ' => 'u', 'ფ' => 'p', 'ქ' => 'k', 'ღ' => 'g', 'ყ' => 'q', 'შ' => 's',
+            'ჩ' => 'c', 'ც' => 't', 'ძ' => 'd', 'წ' => 't', 'ჭ' => 'c', 'ხ' => 'k',
+            'ჯ' => 'j', 'ჰ' => 'h',
+        ];
+
+        $str = str_replace(
+            array_keys($transliteration),
+            array_values($transliteration),
+            $str
+        );
+
+        return $str;
+    }
+}
